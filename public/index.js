8496484a0c72b0b8cec52ec9e552e319ee86ced9
Matt Larson <matt.a.larson@gmail.com>
2017-07-21 12:48:14 -0500

    added socket.io

--- public/index.js
+++ public/index.js
@@ -1,4 +1,7 @@
-// public/index.js
+var socket = io();
+socket.on('externalMidi', gotExternalMidiMessage);
+
+// midi oscillator playback
 var context = new AudioContext();
 var oscillators = {};
 var midi, data;
@@ -24,3 +27,54 @@ allInputs.next()) {
 function onMIDIFailure() {
   console.warn("Not finding a MIDI controller");
 }
+
+// function that listens for midi messages
+function onMIDImessage(messageData) {
+  socket.emit('midi', note);
+  var newItem = document.createElement('li');
+  newItem.appendChild(document.createTextNode(messageData.data));
+  newItem.className = 'user-midi';
+  document.getElementById('midi-data').prepend(newItem);
+  var d = messageData.data; // Example: [144, 60, 100]
+  var note = {
+    on: d[0],
+    pitch: d[1],
+    velocity: d[2]
+  };
+  play(note);
+}
+
+// function to play the note object that is passed in
+function play(note){
+    switch(note.on) {
+      case 144:
+        noteOn(frequency(note.pitch), note.velocity);
+        break;
+      case 128:
+        noteOff(frequency(note.pitch), note.velocity);
+        break;
+    }
+    function frequency(note) {
+      return Math.pow(2, ((note - 69) / 12)) * 440;
+    }
+    function noteOn(frequency, velocity) {
+      var osc = oscillators[frequency] = context.createOscillator();
+        osc.type = 'sawtooth';
+        osc.frequency.value = frequency;
+        osc.connect(context.destination);
+        osc.start(context.currentTime);
+    }
+    function noteOff(frequency, velocity) {
+        oscillators[frequency].stop(context.currentTime);
+        oscillators[frequency].disconnect();
+    }
+  }
+
+  // function to play any extrenal notes that come in from other users
+  function gotExternalMidiMessage(data) {
+    var newItem = document.createElement('li');
+      newItem.appendChild(document.createTextNode('Note: ' + data.pitch + ' Velocity: ' + data.velocity));
+      newItem.className = "external-midi";
+      document.getElementById('midi-data').prepend(newItem);
+      playNote(data);
+  } // end gotExternalMidiMessage
