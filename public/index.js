8fd203c6dedf066061c6aaf160d0f3fc5672c416
Matt Larson <matt.a.larson@gmail.com>
2017-07-21 11:34:10 -0500

    connecting midi controller with Web MIDI API

--- public/index.js
+++ public/index.js
@@ -1,2 +1,26 @@
 // public/index.js
-console.log('index.js is connected!');
+var context = new AudioContext();
+var oscillators = {};
+var midi, data;
+
+if (navigator.requestMIDIAccess) {
+  navigator.requestMIDIAccess({
+    sysex: false
+  }).then(onMIDISuccess, onMIDIFailure);
+} else {
+  console.warn("No MIDI support in your browser");
+}
+
+function onMIDISuccess(midiData) {
+  console.log(midiData);
+  midi = midiData;
+  var allInputs = midi.inputs.values();
+  for (var input = allInputs.next(); input && !input.done; input =
+allInputs.next()) {
+
+  }
+}
+
+function onMIDIFailure() {
+  console.warn("Not finding a MIDI controller");
+}
