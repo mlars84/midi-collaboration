ad04720a1bdc3c3dfc3fa3818707b170c604fad9
Matt Larson <matt.a.larson@gmail.com>
2017-07-25 15:06:43 -0500

    got keyboard working, changed background

--- server.js
+++ server.js
@@ -4,3 +4,18 @@ const port = process.env.PORT || 8080;
 var server = app.listen(port);
 
 app.use(express.static('public'));
+
+var socket = require('socket.io');
+var io = socket(server);
+
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket) {
+  console.log('new connection from:' + socket.id);
+
+  socket.on('midi', midiMsg);
+  function midiMsg(data) {
+    socket.broadcast.emit('externalMidi', data);
+    console.log(socket.id, ' sending data to other users: ', data);
+  }
+}
